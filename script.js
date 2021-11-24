028780281daba861ea529c70f1ebea646e22a49a
ForrestGumpingIT <92694825+ForrestGumpingIT@users.noreply.github.com>
2021-11-24 15:07:01 +0000

    initial upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function count(){
+var count=99;
+while(count>=0){
+		document.getElementById("main").innerHTML+="<br/>"+count+"bottles o beer on the wall, ... bottles of beer
+Take one down and pass it around, How many bottles o beer on the wall now";
+ count=count-1;
+ 
+}
+}
